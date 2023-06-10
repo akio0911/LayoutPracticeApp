0ebf18f5f8d876e77fc71bd2390a669016654ae9
Ryuga <ribereo.minami@gmail.com>
2023-06-10 09:40:17 +0900

    Initial Commit

---
+++ LayoutPracticeApp/LayoutPracticeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutPracticeAppApp.swift
+//  LayoutPracticeApp
+//
+//  Created by Ryuga on 2023/06/10.
+//
+
+import SwiftUI
+
+@main
+struct LayoutPracticeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

8dc40605ca48afa53930905d10837f1a0b9b6f97
Ryuga <ribereo.minami@gmail.com>
2023-06-10 22:51:10 +0900

    feat: NavigationStackのpathで使用するために、ScreenNamesで各画面をcaseで定義した。

---
+++ LayoutPracticeApp/Model/ScreenNames.swift
@@ -0,0 +1,15 @@
+//
+//  ScreenNames.swift
+//  LayoutPracticeApp
+//
+//  Created by Ryuga on 2023/06/10.
+//
+
+import Foundation
+
+enum ScreenNames: Hashable {
+    case home
+    case preparation
+    case question
+    case end
+}
